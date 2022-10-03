f99388c0f5b52717c134ba991df2587370dc93b4
Zach <zachary.brown@vtsd.com>
2022-10-03 12:03:02 -0400

    Lab done

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,16 +12,20 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
-
-
-
-
-
-
-
-
-
-        System.out.println();
+        double numMonths  = numYears * 12;
+        double monthlyRate = annualRate / 100 / 12;
+        double monthlyPay = ((monthlyRate * (Math.pow((1+monthlyRate),numMonths)))/((Math.pow((1+monthlyRate),numMonths)) - 1)) * principal;
+        double totalPay = monthlyPay * numMonths;
+        double totalInterest = totalPay - principal;
+        monthlyPay = Math.round(monthlyPay * 100) / 100.0;
+        totalPay = Math.round(totalPay * 100) / 100.0;
+        totalInterest = Math.round(totalInterest * 100) /100.0;
+        System.out.println("Principal:        $" + principal);
+        System.out.println("Annual Rate:      " + annualRate + "%");
+        System.out.println("Number of Years   " + numYears);
+        System.out.println("Monthly Payment:  $" + monthlyPay);
+        System.out.println("Total Pay:        $" + totalPay);
+        System.out.println("Total Interest:   $" + totalInterest);
     }
 }
 
